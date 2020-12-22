a09c077d16f8e20ae207aeb7642646e0a4ab8cff
Eugene <Zhenya_Oleynik@users.noreply.github.com>
2020-12-22 19:49:43 +0200

    OptionsFile.java added to serialize and deserialize Options.java

--- src/core/Globals.java
+++ src/core/Globals.java
@@ -2,4 +2,5 @@ package core;
 
 public class Globals {
     public static int speed = 2; // 1 - Slow; 2 - Medium; 3 - Fast; 4 - Extremal
+    public static String optionsFile = "src/data/options.dat";
 }
---
+++ src/utils/OptionsFile.java
@@ -0,0 +1,29 @@
+package utils;
+
+import core.Globals;
+import core.Options;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
+public class OptionsFile {
+    public static void serialize(Options options) {
+        try (ObjectOutputStream os = new ObjectOutputStream(new FileOutputStream(Globals.optionsFile))) {
+            os.writeObject(options);
+        }
+        catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+    public static Options deserialize() {
+        try (ObjectInputStream oi = new ObjectInputStream(new FileInputStream(Globals.optionsFile))) {
+            return (Options)oi.readObject();
+        }
+        catch (Exception e) {
+            System.out.println(e.getMessage());
+            return null;
+        }
+    }
+}
