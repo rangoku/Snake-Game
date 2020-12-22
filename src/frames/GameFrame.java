9cb1d0df8fd5f5e87c65ca336082811049cfc2a9
Eugene <Zhenya_Oleynik@users.noreply.github.com>
2020-12-22 19:54:27 +0200

    Revert "Globals set from options file on appear & handle window closing"
    This reverts commit 16f38e9e

--- src/frames/GameFrame.java
+++ src/frames/GameFrame.java
@@ -1,13 +1,8 @@
 package frames;
 
-import core.Globals;
-import core.Options;
 import core.SwingRouter.Router;
-import utils.OptionsFile;
 
 import javax.swing.*;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
 
 public class GameFrame extends JFrame {
 
@@ -18,15 +13,6 @@ public class GameFrame extends JFrame {
     private void initFrame() {
 
         Router.setMainFrame(this); // set current frame as main to work with him in future
-        Options fromFile = OptionsFile.deserialize();
-        Globals.speed = fromFile == null ? Globals.speed : fromFile.getSpeed();
-
-        addWindowListener(new WindowAdapter() {
-            @Override
-            public void windowClosing(WindowEvent e) {
-                OptionsFile.serialize(new Options(Globals.speed));
-            }
-        });
 
         add(new MenuFrame()); // starts from menu
 
