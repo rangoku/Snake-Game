f1325085b0261cc45b5a77a1a4cb80b74f876b8a
Eugene <Zhenya_Oleynik@users.noreply.github.com>
2020-12-26 19:53:38 +0200

    Achievements.java added

---
+++ src/serializable/Achievements.java
@@ -0,0 +1,12 @@
+package serializable;
+
+import java.io.Serializable;
+
+public class Achievements implements Serializable {
+
+    private int score;
+
+    public int getScore() {
+        return score;
+    }
+}
